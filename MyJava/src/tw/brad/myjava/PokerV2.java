4bc69ddfc3882228e9a5733f8cbcf2d2045f3044
BradChao <brad@brad.tw>
2022-10-17 15:49:57 +0800

    10171549

--- MyJava/src/tw/brad/myjava/PokerV2.java
+++ MyJava/src/tw/brad/myjava/PokerV2.java
@@ -1,5 +1,7 @@
 package tw.brad.myjava;
 
+import java.util.Arrays;
+
 public class PokerV2 {
 
 	public static void main(String[] args) {
@@ -23,10 +25,30 @@ public class PokerV2 {
 			}while (isDup);
 
 			poker[i] = rand;
-			System.out.println(poker[i]);
+			//System.out.println(poker[i]);
 		}
 		System.out.println("---");
 		System.out.println(System.currentTimeMillis() - start);
+		System.out.println("---");
+		
+		int[][] players = new int[4][13];
+		for (int i=0; i<poker.length; i++) {
+			players[i % 4][i / 4] = poker[i];
+		}
+		// A,2,3...J,Q,K,A,2,3......JQK
+		// 0,1,2,3...12, 13,14,15,...25,26,27, ....
+		// 黑桃          紅心           方塊          梅花 
+		String[] symbols = new String[] {"黑桃","紅心","方塊","梅花"};
+		String[] values = {"A ","2 ","3 ","4 ","5 ","6 ","7 ",
+				"8 ","9 ","10","J ","Q ","K ",};
+		
+		for (int[] cards : players) {
+			Arrays.sort(cards);
+			for (int card: cards) {
+				System.out.print(symbols[card / 13] + values[card % 13] + " ");
+			}
+			System.out.println();
+		}
 		
 	}
 
