65b437d699e5acbbdeb33412bbf307699d6f3311
BradChao <brad@brad.tw>
2022-10-21 10:39:56 +0800

    10211039

---
+++ MyJava/src/tw/brad/myclass/MyDrawer.java
@@ -0,0 +1,11 @@
+package tw.brad.myclass;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class MyDrawer extends JPanel {
+	public MyDrawer() {
+		setBackground(Color.YELLOW);
+	}
+}
--- MyJava/src/tw/brad/myjava/MySign.java
+++ MyJava/src/tw/brad/myjava/MySign.java
@@ -1,10 +1,40 @@
 package tw.brad.myjava;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import tw.brad.myclass.MyDrawer;
 
 public class MySign extends JFrame {
+	private JButton clear, undo, redo, save, saveas, load;
+	private MyDrawer myDrawer;
+	
 	public MySign() {
 		super("簽名App");
+		
+		setLayout(new BorderLayout());
+		
+		clear = new JButton("Clear");
+		undo = new JButton("Undo");
+		redo = new JButton("Redo");
+		
+		JPanel top = new JPanel(new FlowLayout());
+		top.add(clear); top.add(undo); top.add(redo);
+		
+		add(top, BorderLayout.NORTH);
+		
+		myDrawer = new MyDrawer();
+		add(myDrawer, BorderLayout.CENTER);
+		
+		
+		
+		
+		
+		
 		
 		setSize(800, 480);
 		setVisible(true);
