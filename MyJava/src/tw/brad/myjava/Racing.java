f967da3a25c612e0e7deab2b8a517e56f9b28e60
BradChao <brad@brad.tw>
2022-10-26 14:50:54 +0800

    10261450

--- MyJava/src/tw/brad/myjava/Racing.java
+++ MyJava/src/tw/brad/myjava/Racing.java
@@ -1,6 +1,8 @@
 package tw.brad.myjava;
 
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -9,6 +11,7 @@ import javax.swing.JLabel;
 public class Racing extends JFrame {
 	private JButton go;
 	private JLabel[] lanes;
+	private Car[] cars;
 	
 	public Racing() {
 		super("賽車");
@@ -21,9 +24,42 @@ public class Racing extends JFrame {
 			add(lanes[i]);
 		}
 		
+		go.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				go();
+			}
+		});
+		
 		setSize(800, 480);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		preGo();
+	}
+	
+	private void preGo() {
+		cars = new Car[8];
+		for (int i=0; i<cars.length; i++) {
+			cars[i] = new Car(i);
+		}
+	}
+	
+	private void go() {
+		for (int i=0; i<cars.length; i++) {
+			cars[i].start();
+		}
+	}
+	
+	private class Car extends Thread {
+		private int lane;
+		Car (int lane){this.lane = lane;}
+		@Override
+		public void run() {
+			for (int i=0; i<100; i++) {
+				lanes[lane].setText(lanes[lane].getText() + ">");
+			}
+		}
 	}
 
 	public static void main(String[] args) {
