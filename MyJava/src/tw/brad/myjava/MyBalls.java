559ea2786114006e45c4b39e8ad7415a6ace1b4a
BradChao <brad@brad.tw>
2022-10-27 09:47:24 +0800

    10270947

--- MyJava/src/tw/brad/myjava/MyBalls.java
+++ MyJava/src/tw/brad/myjava/MyBalls.java
@@ -2,7 +2,15 @@ package tw.brad.myjava;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -10,8 +18,7 @@ public class MyBalls extends JFrame {
 	private MyPanel myPanel;
 	
 	public MyBalls() {
-		super();
-		
+		super("My Game");
 	
 		setLayout(new BorderLayout());
 		myPanel = new MyPanel();
@@ -26,6 +33,29 @@ public class MyBalls extends JFrame {
 	private class MyPanel extends JPanel {
 		public MyPanel() {
 			setBackground(Color.YELLOW);
+			addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					clickEvent(e.getX(), e.getY());
+				}
+			});
+		}
+		
+		private void clickEvent(int x, int y) {
+			
+		}
+		
+		@Override
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			Graphics2D g2d = (Graphics2D)g;
+			
+			try {
+				BufferedImage img = ImageIO.read(new File("dir1/ball0.png"));
+				g.drawImage(img, 0, 0, null);
+			}catch (Exception e) {}
+			
+			
 		}
 	}
 	
