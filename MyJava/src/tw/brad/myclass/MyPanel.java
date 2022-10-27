57e9333b5f144344cb8c0b099257b4e8d9d33733
BradChao <brad@brad.tw>
2022-10-27 11:14:08 +0800

    10271114

--- MyJava/src/tw/brad/myclass/MyPanel.java
+++ MyJava/src/tw/brad/myclass/MyPanel.java
@@ -15,20 +15,58 @@ import javax.swing.JPanel;
 
 public class MyPanel extends JPanel {
 	private Timer timer;
+	private int viewW, viewH;
 	
 	public MyPanel() {
 		setBackground(Color.YELLOW);
+		timer = new Timer();
+		timer.schedule(new RefreshTask(), 0, 16);
 		
+	}
+	
+	private class RefreshTask extends TimerTask {
+		@Override
+		public void run() {
+			repaint();
+		}
 	}
 	
 	
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		viewW = getWidth(); viewH = getHeight();
+		
 		Graphics2D g2d = (Graphics2D)g;
 		
 		//g.drawImage(ballImg, ballX, ballY, null);
 		
 	}
-
+	
 }
+
+class Ball extends TimerTask {
+	private int x, y, dx, dy;
+	private int ball;	// 0, 1, 2
+	private MyPanel myPanel;
+	
+	Ball(int x, int y, MyPanel myPanel){
+		this.x = x; this.y = y;
+		dx = dy = 4;
+		ball = (int)(Math.random()*3);
+		this.myPanel = myPanel;
+	}
+	
+	@Override
+	public void run() {
+		if (x  <= 0 || x + 64 >= myPanel.getWidth()) {
+			dx *= -1;
+		}
+		if (y <= 0 || y + 64 >= myPanel.getHeight()) {
+			dy *= -1;
+		}
+		x += dx;
+		y += dy;	
+	}
+}
+
