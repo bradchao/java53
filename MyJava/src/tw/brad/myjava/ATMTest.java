7c737b9c0e67123a317a27f36e0662e36f0e6ab3
BradChao <brad@brad.tw>
2022-10-28 10:24:18 +0800

    10281024

--- MyJava/src/tw/brad/myjava/ATMTest.java
+++ MyJava/src/tw/brad/myjava/ATMTest.java
@@ -19,18 +19,46 @@ class ATM {
 		}
 	}
 	void getMoney(int get, String name) {
-		if (money <= get) {
+		if (money >= get) {
 			money -= get;
-			System.out.println("get:" + get + " => " + money);
+			System.out.println("get:" + get + " => " + money + " : " + name);
 		}else {
 			System.out.println("get:xxx => " + name);
 		}
 	}
+}
+
+class Bank extends Thread{
+	private ATM atm;
+	Bank(ATM atm){this.atm = atm;}
+	
+	@Override
+	public void run() {
+		for (int i=0; i<10; i++) {
+			atm.addMoney(200);
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+			}
+			
+		}
+	}
 	
 }
-class Bank {
-	
-}
-class Person {
-	
+
+class Person extends Thread {
+	private ATM atm;
+	private String name;
+	Person(ATM atm, String name){this.atm = atm; this.name = name;}
+	@Override
+	public void run() {
+		for (int i=0; i<20; i++) {
+			atm.getMoney(10 + (int)(Math.random()*50), name);
+			
+			try {
+				Thread.sleep(100);
+			}catch(Exception e) {}
+		}
+	}
 }
